02cf11c8143d72f56c509898b21c77cdd3d3933e
nx006 <limazero14@gmail.com>
2024-02-05 03:39:16 +0900

    feat: detail 관련 함수 생성

--- lib/category/repository/category_repository.dart
+++ lib/category/repository/category_repository.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:safetyedu/category/model/category_detail_model.dart';
 import 'package:safetyedu/category/model/category_model.dart';
 import 'package:safetyedu/common/model/model_list.dart';
 import 'package:safetyedu/common/model/model_list_meta.dart';
+import 'package:safetyedu/common/model/model_with_id.dart';
 import 'package:safetyedu/common/repository/model_list_repository_interface.dart';
 
 final categoryRepositoryProvider = Provider<CategoryRepository>(
@@ -9,29 +11,31 @@ final categoryRepositoryProvider = Provider<CategoryRepository>(
 );
 
 class CategoryRepository implements IModelListRepository<CategoryModel> {
+  final List<CategoryModel> items = [
+    const CategoryModel(
+      id: "1",
+      title: 'Earthquake',
+      description: 'What should we do when Earthquake happens?',
+      imageUrl: 'images/earthquake.png',
+      solvedQuizCount: 5,
+      totalQuizCount: 20,
+      categoryDescription: 'This is Earthquake category.',
+    ),
+    const CategoryModel(
+      id: "2",
+      title: 'Tsunami',
+      description: 'What should we do when Tsunami comes?',
+      imageUrl: 'images/tsunami.png',
+      solvedQuizCount: 5,
+      totalQuizCount: 20,
+      categoryDescription: 'This is Tsunami category.',
+    ),
+  ];
+
   @override
   Future<ModelList<CategoryModel>> fetch() async {
     // TODO: implement fetch
     // 현재는 0.5초 후 더미 데이터 반환
-
-    final List<CategoryModel> items = [
-      const CategoryModel(
-        id: "1",
-        title: 'Earthquake',
-        description: 'What should we do when Earthquake happens?',
-        imageUrl: 'images/earthquake.png',
-        solvedQuizCount: 5,
-        totalQuizCount: 20,
-      ),
-      const CategoryModel(
-        id: "2",
-        title: 'Tsunami',
-        description: 'What should we do when Tsunami comes?',
-        imageUrl: 'images/tsunami.png',
-        solvedQuizCount: 5,
-        totalQuizCount: 20,
-      ),
-    ];
 
     final meta = ModelListMeta(count: items.length);
 
@@ -40,6 +44,25 @@ class CategoryRepository implements IModelListRepository<CategoryModel> {
       () => ModelList(
         items: items,
         meta: meta,
+      ),
+    );
+  }
+
+  Future<CategoryDetailModel> getCategoryDetail({required Id id}) {
+    final category = items.firstWhere((element) => element.id == id);
+
+    return Future.delayed(
+      const Duration(milliseconds: 500),
+      () => CategoryDetailModel(
+        id: category.id,
+        title: category.title,
+        description: category.description,
+        imageUrl: category.imageUrl,
+        solvedQuizCount: category.solvedQuizCount,
+        totalQuizCount: category.totalQuizCount,
+        categoryDescription: category.categoryDescription,
+        detail: 'This is detail of ${category.title}',
+        quizzes: const [],
       ),
     );
   }

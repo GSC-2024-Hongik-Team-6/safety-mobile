6fe6f6d060647f9eb8645f3e5c39b53696f7d171
nx006 <limazero14@gmail.com>
2024-02-06 19:11:17 +0900

    fix: CategoryDetailModel 관련 문제 해결
    초반에 getDetail을 하지 않아서 생기는 문제였음
    
    ```
      @override
      void initState() {
        super.initState();
    
        ref.read(categoryProvider.notifier).getDetail(id: widget.cid);
      }
    ```
    위 사항을 추가해서 해결

--- lib/category/provider/category_provider.dart
+++ lib/category/provider/category_provider.dart
@@ -8,7 +8,7 @@ import 'package:safetyedu/common/model/model_with_id.dart';
 import 'package:collection/collection.dart';
 
 final categoryDetailProvider =
-    Provider.family<CategoryDetailModel?, Id>((ref, Id id) {
+    Provider.family<CategoryModel?, Id>((ref, Id id) {
   final state = ref.watch(categoryProvider);
 
   if (state is! ModelList) {

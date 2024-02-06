fef5b3f75be6b3470f9dc072a47e42c890badaad
nx006 <limazero14@gmail.com>
2024-02-06 19:22:53 +0900

    refactor{view}: Loading State 조건 통합

--- lib/category/view/category_detail_screen.dart
+++ lib/category/view/category_detail_screen.dart
@@ -52,16 +52,6 @@ class _CategoryDetailScreenState extends ConsumerState<CategoryDetailScreen> {
       );
     }
 
-    if (category is! CategoryDetailModel) {
-      return const DefaultLayout(
-        child: Center(
-          child: Text(
-            'Error: Is not a Detail Model',
-          ),
-        ),
-      );
-    }
-
     return DefaultLayout(
       title: 'Learning by Quiz',
       appBar: _buildCategoryAppBar(context: context, category: category),

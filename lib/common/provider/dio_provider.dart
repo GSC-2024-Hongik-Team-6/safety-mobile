6c09068207319378940dcec5aa448e024318aa3d
nx006 <limazero14@gmail.com>
2024-02-13 02:27:04 +0900

    feat{dio}: dio logger  추가

--- lib/common/provider/dio_provider.dart
+++ lib/common/provider/dio_provider.dart
@@ -1,11 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:safetyedu/common/const/data.dart';
 
 final dioProvider = Provider<Dio>(
-  (ref) => Dio(
-    BaseOptions(
-      baseUrl: apiurl,
-    ),
-  ),
+  (ref) {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: apiurl,
+      ),
+    );
+
+    final logger = PrettyDioLogger();
+
+    dio.interceptors.add(logger);
+
+    return dio;
+  },
 );

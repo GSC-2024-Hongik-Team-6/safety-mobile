1f8c1a707430dcbda2cbf888c55b2f230d3fe398
nx006 <limazero14@gmail.com>
2024-02-13 00:17:34 +0900

    fix{user}: Android 상에서 키보드 펼쳐질 때 화면을 가리는 문제 수정

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -29,138 +29,165 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     final currentUserState = ref.watch(currentUserProvider);
 
     return DefaultLayout(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 100.0),
-                const Text(
-                  'LOGIN',
-                  style: CustomTextStyle(
-                    textFontSize: 24,
-                    textFontWeight: FontWeight.w700,
-                    textColor: titleTextColor,
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                CustomTextFormField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  hintText: 'Email',
-                ),
-                const SizedBox(height: 12.0),
-                CustomTextFormField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  hintText: 'Password',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 12.0),
-                // 로그인 버튼
-                ElevatedButton(
-                  onPressed: currentUserState is UserLoading
-                      ? null
-                      : () {
-                          ref.read(currentUserProvider.notifier).login(
-                                email: email,
-                                password: password,
-                              );
-                        },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: primaryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text(
-                      currentUserState is UserLoading
-                          ? 'Wait for a second...'
-                          : 'LOGIN',
-                      style: const CustomTextStyle(
-                        textFontWeight: FontWeight.w600,
-                        textFontSize: 18,
-                        textColor: Colors.white,
+      safeAreaDisabled: true,
+      child: CustomScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 60.0),
+                    const Text(
+                      'LOGIN',
+                      style: CustomTextStyle(
+                        textFontSize: 24,
+                        textFontWeight: FontWeight.w700,
+                        textColor: titleTextColor,
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 12.0),
-                if (currentUserState is UserError)
-                  Text(
-                    currentUserState.message,
-                    style: const CustomTextStyle(
-                      textFontSize: 16,
-                      textColor: Colors.red,
+                    const SizedBox(height: 20.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          CustomTextFormField(
+                            onChanged: (value) {
+                              email = value;
+                            },
+                            hintText: 'Email',
+                          ),
+                          const SizedBox(height: 12.0),
+                          CustomTextFormField(
+                            onChanged: (value) {
+                              password = value;
+                            },
+                            hintText: 'Password',
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 12.0),
+                          // 로그인 버튼
+                          ElevatedButton(
+                            onPressed: currentUserState is UserLoading
+                                ? null
+                                : () {
+                                    ref
+                                        .read(currentUserProvider.notifier)
+                                        .login(
+                                          email: email,
+                                          password: password,
+                                        );
+                                  },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                            ),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 10.0),
+                              child: Text(
+                                currentUserState is UserLoading
+                                    ? 'Wait for a second...'
+                                    : 'LOGIN',
+                                style: const CustomTextStyle(
+                                  textFontWeight: FontWeight.w600,
+                                  textFontSize: 18,
+                                  textColor: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-              ],
-            ),
-          ),
-          Column(
-            children: [
-              SocialLoginButton(
-                buttonType: SocialLoginButtonType.google,
-                onPressed: () {},
-                fontSize: 18,
-                borderRadius: 12.0,
-              ),
-              const SizedBox(height: 12.0),
-              SocialLoginButton(
-                buttonType: SocialLoginButtonType.github,
-                onPressed: () {},
-                fontSize: 18.0,
-                borderRadius: 12.0,
-              ),
-              const SizedBox(height: 12.0),
-              SocialLoginButton(
-                buttonType: SocialLoginButtonType.apple,
-                onPressed: () {},
-                fontSize: 18.0,
-                borderRadius: 12.0,
-              ),
-            ],
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                style: const CustomTextStyle(
-                  textFontSize: 16,
-                  textColor: inputHintTextColor,
-                ),
-                children: [
-                  const TextSpan(
-                    text: 'If not yet a member, ',
-                  ),
-                  TextSpan(
-                    text: 'Sign Up',
-                    style: const TextStyle(
-                      color: primaryColor,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(height: 12.0),
+                    if (currentUserState is UserError)
+                      Text(
+                        currentUserState.message,
+                        style: const CustomTextStyle(
+                          textFontSize: 16,
+                          textColor: Colors.red,
+                        ),
+                      ),
+                    const SocialLoginButtonList(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 20),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          style: const CustomTextStyle(
+                            textFontSize: 16,
+                            textColor: inputHintTextColor,
+                          ),
+                          children: [
+                            const TextSpan(
+                              text: 'If not yet a member, ',
+                            ),
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: const TextStyle(
+                                color: primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  context.pushNamed(
+                                    SignUpScreen.routeName,
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        context.pushNamed(
-                          SignUpScreen.routeName,
-                        );
-                      },
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ],
       ),
-    ));
+    );
+  }
+}
+
+class SocialLoginButtonList extends StatelessWidget {
+  const SocialLoginButtonList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SocialLoginButton(
+          buttonType: SocialLoginButtonType.google,
+          onPressed: () {},
+          fontSize: 18,
+          borderRadius: 12.0,
+        ),
+        const SizedBox(height: 12.0),
+        SocialLoginButton(
+          buttonType: SocialLoginButtonType.github,
+          onPressed: () {},
+          fontSize: 18.0,
+          borderRadius: 12.0,
+        ),
+        const SizedBox(height: 12.0),
+        SocialLoginButton(
+          buttonType: SocialLoginButtonType.apple,
+          onPressed: () {},
+          fontSize: 18.0,
+          borderRadius: 12.0,
+        ),
+      ],
+    );
   }
 }
--- lib/user/view/signup_screen.dart
+++ lib/user/view/signup_screen.dart
@@ -28,102 +28,113 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
     final currentUserState = ref.watch(currentUserProvider);
 
     return DefaultLayout(
+      safeAreaDisabled: true,
       title: '',
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const SizedBox(height: 60.0),
-                  const Text(
-                    'REGISTER',
-                    style: CustomTextStyle(
-                      textFontSize: 24,
-                      textFontWeight: FontWeight.w700,
-                      textColor: titleTextColor,
+      child: CustomScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 60.0),
+                    const Text(
+                      'REGISTER',
+                      style: CustomTextStyle(
+                        textFontSize: 24,
+                        textFontWeight: FontWeight.w700,
+                        textColor: titleTextColor,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  CustomTextFormField(
-                    onChanged: (value) {
-                      name = value;
-                    },
-                    hintText: 'Name',
-                  ),
-                  const SizedBox(height: 12.0),
-                  CustomTextFormField(
-                    onChanged: (value) {
-                      email = value;
-                    },
-                    hintText: 'Email',
-                  ),
-                  const SizedBox(height: 12.0),
-                  CustomTextFormField(
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    hintText: 'Password',
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 12.0),
-                  CustomTextFormField(
-                    onChanged: (value) {
-                      checkingPassword = value;
-                    },
-                    hintText: 'Checking Password',
-                    obscureText: true,
-                  ),
-                ],
+                    const SizedBox(height: 20.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          CustomTextFormField(
+                            onChanged: (value) {
+                              name = value;
+                            },
+                            hintText: 'Name',
+                          ),
+                          const SizedBox(height: 12.0),
+                          CustomTextFormField(
+                            onChanged: (value) {
+                              email = value;
+                            },
+                            hintText: 'Email',
+                          ),
+                          const SizedBox(height: 12.0),
+                          CustomTextFormField(
+                            onChanged: (value) {
+                              password = value;
+                            },
+                            hintText: 'Password',
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 12.0),
+                          CustomTextFormField(
+                            onChanged: (value) {
+                              checkingPassword = value;
+                            },
+                            hintText: 'Checking Password',
+                            obscureText: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                    if (currentUserState is UserError)
+                      Text(
+                        currentUserState.message,
+                        style: const CustomTextStyle(
+                          textFontSize: 16,
+                          textFontWeight: FontWeight.w400,
+                          textColor: Colors.red,
+                        ),
+                      ),
+                    // 회원 가입 버튼
+                    ElevatedButton(
+                      onPressed: currentUserState is UserLoading
+                          ? null
+                          : () {
+                              ref.read(currentUserProvider.notifier).signUp(
+                                    username: name,
+                                    email: email,
+                                    password: password,
+                                    passwordConfirm: checkingPassword,
+                                  );
+                              if (currentUserState is UserModel) {
+                                context.pop();
+                              }
+                            },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        child: Text(
+                          'REGISTER',
+                          style: CustomTextStyle(
+                            textFontSize: 16,
+                            textFontWeight: FontWeight.w700,
+                            textColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            if (currentUserState is UserError)
-              Text(
-                currentUserState.message,
-                style: const CustomTextStyle(
-                  textFontSize: 16,
-                  textFontWeight: FontWeight.w400,
-                  textColor: Colors.red,
-                ),
-              ),
-            // 회원 가입 버튼
-            ElevatedButton(
-              onPressed: currentUserState is UserLoading
-                  ? null
-                  : () {
-                      ref.read(currentUserProvider.notifier).signUp(
-                            username: name,
-                            email: email,
-                            password: password,
-                            passwordConfirm: checkingPassword,
-                          );
-                      if (currentUserState is UserModel) {
-                        context.pop();
-                      }
-                    },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Text(
-                  'REGISTER',
-                  style: CustomTextStyle(
-                    textFontSize: 16,
-                    textFontWeight: FontWeight.w700,
-                    textColor: Colors.white,
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

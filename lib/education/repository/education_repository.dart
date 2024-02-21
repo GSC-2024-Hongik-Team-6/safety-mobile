55d16bbe7830ac03d90bc463c136ab9068d8efac
nx006 <limazero14@gmail.com>
2024-02-21 16:02:38 +0900

    feat: Education의 api 재변경 반영, 상속 구조 적용

--- lib/education/repository/education_repository.dart
+++ lib/education/repository/education_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:safetyedu/common/model/model_with_id.dart';
 import 'package:safetyedu/education/model/education_model.dart';
 import 'package:safetyedu/common/model/model_list.dart';
 import 'package:safetyedu/common/provider/dio_provider.dart';
@@ -18,10 +19,16 @@ final educationRepositoryProvider = Provider<EducationRepository>(
 
 @RestApi(baseUrl: '/education')
 abstract class EducationRepository
-    implements IModelListRepository<EducationModel> {
+    implements IDetailRepository<EducationModel> {
   factory EducationRepository(Dio dio) = _EducationRepository;
 
   @override
   @GET('/')
   Future<ModelList<EducationModel>> fetch();
+
+  @override
+  @GET('/{id}')
+  Future<EducationModel> getDetail({
+    @Path() required Id id,
+  });
 }
